9ac7660d8c74ca9c243d98ec85dc8a46e111dd45
Nemo Oudeis <david.a.frey@rakuten.com>
2020-05-30 18:13:03 +0900

    add branch filtering

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir build
+echo "Hello from branch: $CIRCLE_BRANCH"
 echo "this should go into the workspace" > build/output.txt
 echo "this should not go into the workspace" > build/output.other
